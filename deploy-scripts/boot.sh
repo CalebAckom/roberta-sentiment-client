60f48fb949b114431288f3703db71b4568af0de5
CalebAckom <caleb.ackom@amalitech.com>
2023-08-17 13:21:55 +0000

    Refactor

--- deploy-scripts/boot.sh
+++ deploy-scripts/boot.sh
@@ -2,4 +2,4 @@
 
 echo "Running new container"
 DOCKER_IMAGE=calebackom/roberta-frontend
-docker run -d -p 3000:3000 $DOCKER_IMAGE --name roberta-frontend
+docker run -d -p 3000:3000 --name roberta-frontend $DOCKER_IMAGE
